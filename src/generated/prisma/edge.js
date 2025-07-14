a1d4177a9ae8a91f15c16cd482fbf794106598d3
Tendriy <anissa.andrianindrina@student.passerellesnumeriques.org>
2025-07-14 08:49:34 +0300

    apprendre modification vien le back

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -211,7 +211,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/home/tantely/Desktop/annissa/plateform-api/src/generated/prisma",
+      "value": "/home/tendry/Bureau/plateform/plateform-api/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -225,7 +225,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/home/tantely/Desktop/annissa/plateform-api/prisma/schema.prisma",
+    "sourceFilePath": "/home/tendry/Bureau/plateform/plateform-api/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -212,7 +212,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/home/tantely/Desktop/annissa/plateform-api/src/generated/prisma",
+      "value": "/home/tendry/Bureau/plateform/plateform-api/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -226,7 +226,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/home/tantely/Desktop/annissa/plateform-api/prisma/schema.prisma",
+    "sourceFilePath": "/home/tendry/Bureau/plateform/plateform-api/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
